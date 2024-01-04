eb31493448093987b8216405f79795e6db821d2d
Rafael Tavares <83890454+Rafael02tavares@users.noreply.github.com>
2024-01-03 23:29:56 -0300

    teste

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main >
+      <div>
+        <p>cabeçalho</p>
+      </div>
+      <div>
+          <p>login</p>
+          <div>
+              <p>busco emprego</p>
+          </div>
+          <div>
+                <p>ofereço vaga de emprego</p>
+                oioioi
+          </div>
+      </div>
+    </main>
+  )
+}
